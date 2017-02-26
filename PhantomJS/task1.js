b360deb52ed3eb9014ad7c5a9b33446707d615f5
dcbryant <dcbryant753@gmail.com>
2017-02-26 17:40:46 +0800

    improve

--- PhantomJS/task1.js
+++ PhantomJS/task1.js
@@ -1,43 +1,44 @@
-var webPage = require('webpage');
-var page = webPage.create();
-var system = require('system');
-var keyword = system.args[1];
-var url = 'https://www.baidu.com/s?wd=' + keyword;
-var dataList = [];
+var page = require('webpage').create();
+var keyWord = require('system').args[1];
+var url = 'http://www.baidu.com/s?wd=' + keyWord;
 var result = {};
-var startTime = Date.now();
+var	beginTime = Date.now();
 
-page.open(url,function(s){
-    if(s == 'fail'){
-        console.log('无法打开链接');
-        result.code = 0;
-        result.msg = '抓取失败'
-        console.log(JSON.stringify(result));
-        plantom.exit();
-    }else if(s == 'success' && page.loadingProgress >= 100){//大于100则抓取成功
-        Console.log('已打开链接,正在抓取');
-        page.includeJs('http://cdn.bootcss.com/jquery/3.1.1/jquery.min.js', function(){
-            dataList = page.evaluate(function(){
-                var data = [];
-                var $conotent = $('.c-container');
-                $conotent.each(function(index){
-                    title:$(this).find('.t').text()||'',
-                    info:$(this).find('.c-abstract').text()||'',
-                    link:$(this).find('.c-showurl').text()||'',
-                    pic:$(this).find('.general_image_pic img').attr('src')||''}
+page.open(url, function(s) {
+	if (s !== 'success') {
+		console.log("open failed!");
+		result.code = 0;
+		result.msg = '抓取失败!';
+		console.log(JSON.stringify(result));
+		phantom.exit();
+	}else if(s === 'success' && s == 'success' && page.loadingProgress >= 100){
+        console.log('open successed');
+        console.log('正在抓取');
+        page.includeJs("http://cdn.bootcss.com/jquery/3.1.1/jquery.min.js", function() {
+            var dataList = page.evaluate(function() {
+                var data = []
+                var $result = $('.result');
+                $result.each(function() {
+                    var imgs = $(this).find('.c-img')
+                    data.push({
+                        title: $(this).children('h3').text(),
+                        info: $(this).find('.c-abstract').text(),
+                        link: $(this).find('h3 a')[0].href,
+                        pic: imgs.length > 0 ? imgs[0].src : ''
+                    })
                 })
                 return data;
             })
-            result = {
-                code:0,
-                keyword:keyword,
-                msg:'抓取成功',
-                time:Date.now() - startTime,
-                dataList:dataList
+            var data = {
+                code: 1,
+                msg: '抓起成功',
+                word: keyWord,
+                time: Date.now() - beginTime,
+                dataList: dataList
             }
-            console.log(JSON.stringify(result));
+            phantom.outputEncoding = 'gb2312';
+            console.log(JSON.stringify(data));
             phantom.exit();
-        }
-
+        })
     }
-});
+})
